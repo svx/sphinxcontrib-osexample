b530a06bd1e94c9ecbcb61b8b6a636791b54022e
sven <sven@so36.net>
2015-12-21 08:05:11 +0100

    More work on lexer

--- lexer/ubuntu.py
+++ lexer/ubuntu.py
@@ -3,7 +3,7 @@ import re
 from pygments.lexer import RegexLexer, bygroups
 from pygments.token import Keyword, Number, Text, Comment
 
-__all__ = ['UbuntuLexer']
+__all__ = ['UbuntuLexer', 'CentosLexer', 'FedoraLexer']
 
 class UbuntuLexer(RegexLexer):
     name = 'Ubuntu'
@@ -19,4 +19,39 @@ class UbuntuLexer(RegexLexer):
 		(r'\S+', Keyword.Pseudo),
 		(r'[;#].*', Comment.Single),
 	    ],
+    }
+class CentosLexer(RegexLexer):
+    name = 'Centos'
+    aliases = ['centos']
+    filenames = ['*.ubuntu']
+
+    tokens = {
+        'root': [
+        (r'\byum\b', Keyword),
+        (r'\b((yum)?update\b)', Keyword),
+        (r'\b((yum)?install\b)', Keyword),
+        (r'\b((yum-)?check\b)', Keyword),
+        (r'\s', Text),
+        (r'-', Text),
+        (r'\S+', Keyword.Pseudo),
+        (r'[;#].*', Comment.Single),
+        ],
+    }
+
+class FedoraLexer(RegexLexer):
+    name = 'Fedora'
+    aliases = ['fedora']
+    filenames = ['*.ubuntu']
+
+    tokens = {
+        'root': [
+        (r'\bdnf\b', Keyword),
+        (r'\b((dnf)?update\b)', Keyword),
+        (r'\b((dnf)?install\b)', Keyword),
+        (r'\b((dnf-)?check\b)', Keyword),
+        (r'\s', Text),
+        (r'-', Text),
+        (r'\S+', Keyword.Pseudo),
+        (r'[;#].*', Comment.Single),
+        ],
     }
